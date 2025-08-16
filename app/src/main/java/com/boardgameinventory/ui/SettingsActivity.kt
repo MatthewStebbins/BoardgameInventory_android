d9f16a1afc6e66b85dfb887e7ba4a663ae5f8fcc
Matthew Stebbins <mattcatt89@yahoo.com>
2025-08-16 17:35:30 -0500

    feat: remove unused location info TextView from bulk upload layout

--- app/src/main/java/com/boardgameinventory/ui/SettingsActivity.kt
+++ app/src/main/java/com/boardgameinventory/ui/SettingsActivity.kt
@@ -2,11 +2,9 @@ package com.boardgameinventory.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import com.boardgameinventory.BuildConfig
 import com.boardgameinventory.R
