b5cbc86c09940d9c88383051b9bbd532a9987896
Matthew Stebbins <mattcatt89@yahoo.com>
2025-08-18 20:09:22 -0500

    chore: update minimum SDK version to 28 and remove unused XML namespace

--- app/src/main/java/com/boardgameinventory/ui/SettingsActivity.kt
+++ app/src/main/java/com/boardgameinventory/ui/SettingsActivity.kt
@@ -100,11 +100,6 @@ class SettingsActivity : BaseAdActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        //applyTextDarkness()
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
         return true
